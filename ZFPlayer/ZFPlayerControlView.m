5cad0fc835b18c01719d8a84f182fdc217e56969
Duong Thanh Tung <tungdt@gemvietnam.com>
2017-12-01 06:05:31 +0700

    Change text on load fail

--- ZFPlayer/ZFPlayerControlView.m
+++ ZFPlayer/ZFPlayerControlView.m
@@ -794,7 +794,7 @@ static const CGFloat ZFPlayerControlBarAutoFadeOutTimeInterval = 0.35f;
 - (UIButton *)failBtn {
     if (!_failBtn) {
         _failBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-        [_failBtn setTitle:@"加载失败,点击重试" forState:UIControlStateNormal];
+        [_failBtn setTitle:@"Video load failed, click to retry please" forState:UIControlStateNormal];
         [_failBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
         _failBtn.titleLabel.font = [UIFont systemFontOfSize:14.0];
         _failBtn.backgroundColor = RGBA(0, 0, 0, 0.7);
